c64a10726a3aa414ebca8bfec10dee4dc6766f0d
theRightHoopRod <unknown.rdm@gmail.com>
2026-06-02 15:11:36 -0600

    Added Point structure to create cities and fn to get distance. Changes to be committed: 	modified:   main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+impl Point {}
+
+// struct FixedGrid {
+//     matrix: [[Point; 20]; 100],
+// }
+
+impl Point {
+    fn get_distance(&self, point: &Point) -> f64 {
+        (((&point.x - &self.x).pow(2) + (&point.y - &self.y).pow(2)) as f64).sqrt()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let prim = Point { x: 5, y: 10 };
+
+    let hola = prim.get_distance(&Point { x: 15, y: 20 });
+
+    println!("{}", &hola);
 }
